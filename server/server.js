db49d2d4ceacce7f4bc664f1bc39443b45dad7d4
Vincent Moore <debonair@Malenas-iMac.att.net>
2015-04-22 08:27:16 -0400

    push it to the limit

--- server/server.js
+++ server/server.js
@@ -6,12 +6,12 @@ var methodOverride = require('method-override');
 var FLKR = require('./java-bridge');
 var db = new FLKR();
 
-app.use( '/lib', express.static(__dirname + '/public/static') );
+app.use('/static', express.static(__dirname+'/public'));
+app.set('views', __dirname + '/public/views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride());
 app.set('view engine', 'jade');
-
 
 app.get('/api/top_movies', function(req, res){
 	db.getTopMovies(function(data){
@@ -20,31 +20,31 @@ app.get('/api/top_movies', function(req, res){
 });
 
 app.get('/api/get_movie', function(req, res){
-	db.getMovie(req.query.title, function(data){
+	db.getMovie(req.query.name, function(data){
 		res.json(data);
 	});
 });
 
 app.get('/api/get_genre', function(req, res){
-	db.getGenre(req.query.genre, function(data){
+	db.getGenre(req.query.name, function(data){
 		res.json(data);
 	});
 });
 
 app.get('/api/get_director', function(req, res){
-	db.getDirector(req.query.director, function(data){
+	db.getDirector(req.query.name, function(data){
 		res.json(data);
 	});
 });
 
 app.get('/api/get_actor', function(req, res){
-	db.getActor(req.query.actor, function(data){
+	db.getActor(req.query.name, function(data){
 		res.json(data);
 	});
 });
 
 app.get('/api/get_tag', function(req, res){
-	db.getTag(req.query.tag, function(data){
+	db.getTag(req.query.name, function(data){
 		res.json(data);
 	});
 });
@@ -62,7 +62,7 @@ app.get('/api/top_actors', function(req, res){
 });
 
 app.get('/', function(req, res){
-	res.render('../views/home');
+	res.render('home');
 });
 http.listen(process.env.PORT, function(){
 	console.log("Server running at port %s", process.env.PORT);
